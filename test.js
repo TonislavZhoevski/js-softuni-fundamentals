5fd40df4e69dadb248bb36349bb317ad77579968
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-08-11 23:57:18 +0300

    second commit

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
-function concatenateData(input) {
-  let firstName = input[0];
-  let lastName = input[1];
-  let age = input[2];
-  let city = input[3];
-  console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${city}`);
+function greenGarten(input) {
+  let qm = input[0];
+  let priceQm = qm * 7.61;
+  let discountValue = 0.18;
+  let discountPrice = discountValue * priceQm;
+  let sumTotal = priceQm - discountPrice;
+  console.log(`The final price is ${sumTotal} lv.\nThe discount is: ${discountPrice} lv.`);
 }
-concatenateData(["Maria", "Ivanova", 20, "Sofia"]);
+greenGarten(["150"]);
